aad3d393bddfea689a15b3b5fcfb84b572b38a60
jonas18121 <jonasxtrm971@gmail.com>
2021-03-14 14:47:05 +0100

    Création de CommentController

---
+++ src/Controller/CommentController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Comment;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class CommentController extends AbstractController
+{
+
+    /**
+     * @Route("/comment/delete/{id}", name="comment_delete", requirements={"id": "\d+"})
+     */
+    public function delete_comment(Comment $comment, EntityManagerInterface $manager)
+    {
+        if (!$this->getUser()) {
+            return $this->redirectToRoute('storage_space_all');
+        }
+        
+        $manager->remove($comment);
+        $manager->flush();
+
+        return $this->redirectToRoute('storage_space_one', [ 'id' => $comment->getStorageSpace()->getId()]);
+    }
+}
