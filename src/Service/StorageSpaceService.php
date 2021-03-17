b6c8650c25145ba224a005271526d94e871a4f50
jonas18121 <jonasxtrm971@gmail.com>
2021-03-17 16:55:26 +0100

    Lorsque le StorageSpaceListener réagi à l'évènnement kernel.request et appel StorageService, on effectu une modification seulement sur les dates du jour

--- src/Service/StorageSpaceService.php
+++ src/Service/StorageSpaceService.php
@@ -17,8 +17,8 @@ class StorageSpaceService
      * StorageSpaceListener fait fonctionner StorageSpaceService::emitStorageCheckDate()
      * qui va rendre un espace de stockage disponible si la date de fin de réservation est passé
      * 
-     * Si la date d'aujourd'hui est plus grand que la date de fin de réservation,
-     * on met la propriété available de StorageSpace en true , pour qu'il soit disponible aux autre user
+     * Si la date d'aujourd'hui est plus grand ou égale à la date de fin de réservation,
+     * on met la propriété available de l'entité StorageSpace en true , pour qu'il soit disponible aux autres user
      */
     public function emitStorageCheckDate(
         Request $response, 
@@ -32,19 +32,41 @@ class StorageSpaceService
         foreach ($bookings as $key => $booking) {
             
             $dateCurrent = new \DateTime();
+
             
-            if ($dateCurrent > $booking->getDateStartAt()) {
+            if ($booking->getDateEndAt()) {
+                
+                if ($dateCurrent >= $booking->getDateEndAt()) {
+                    
+                    /*                              17/03/2021                                 >                                   16/03/2021        retourne +1 jour
+                        soit la date du jour ->diff(new \DateTime()) a au minimun 1 jour de plus que la date en comparaison $booking->getDateEndAt() 
 
-                $storageSpaces = $repoStorage->find_one_booking_in_storage($booking->getId());
+                                                    17/03/2021                                <                                    18/03/2021         retourne -1 jour
+                        soit la date du jour ->diff(new \DateTime()) a au minimun 1 jour de moins que la date en comparaison $booking->getDateEndAt() 
 
-                foreach ($storageSpaces as $key => $storageSpace) {
+                                                    17/03/2021                         ==                    17/03/2021          retourne +0 jour
+                        soit la date du jour ->diff(new \DateTime()) est égale à la date en comparaison $booking->getDateEndAt() 
+                    */
+                    $nb_days_positif_or_negatif = $booking->getDateEndAt()->diff(new \DateTime());
 
-                    $storageSpace->setAvailable(true);
+                    $nb_days = $nb_days_positif_or_negatif->format('%R%a'); //exemple retourne +1 ou -1 ou +0
 
-                    $manager->persist($storageSpace);
-                    $manager->flush();
+                    if ($nb_days == 0) {
+
+                        $storageSpaces = $repoStorage->find_one_booking_in_storage($booking->getId());
+                        
+                        foreach ($storageSpaces as $key => $storageSpace) {
+        
+                            $storageSpace->setAvailable(true);
+        
+                            $manager->persist($storageSpace);
+                            $manager->flush();
+                        }
+                    }
+    
                 }
             }
+            
         }
         
     }
