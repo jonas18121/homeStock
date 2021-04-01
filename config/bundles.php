e14aa777119eab6a32071c6fd9e19de81025d097
jonas <jonasxtrm971@gmail.com>
2021-04-01 15:58:43 +0200

    installation, configuration de VichUploadBundle, permettre à l'entité StorageSpace de recevoir des images

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
 ];
